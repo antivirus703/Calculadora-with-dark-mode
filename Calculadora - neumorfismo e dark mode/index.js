a53ae263a56464f1bb00eebc8c8056ed05af5260
antivirus703 <nortonberbert@gmail.com>
2022-08-30 13:07:02 -0300

    adicionado calculadora web com dark mode

---
+++ Calculadora - neumorfismo e dark mode/index.js
@@ -0,0 +1,27 @@
+let buttons = document.querySelector('.buttons');
+let btn = document.querySelectorAll('span');
+let value = document.getElementById('value');
+let toggleBtn = document.querySelector('.toggleBtn');
+let body = document.querySelector('body');
+
+for(let i=0; i<btn.length; i++) {
+  btn[i].addEventListener("click",function(){
+    if(this.innerHTML == "="){
+      value.innerHTML = eval(value.innerHTML);
+    } else{
+      if(this.innerHTML == "C"){
+        value.innerHTML = "";
+      } else{
+        if(this.innerHTML == "Del"){
+          value.innerHTML = value.innerHTML.slice(0, -1);
+        }else{
+          value.innerHTML += this.innerHTML;
+        }
+      }
+    }
+})
+}
+
+toggleBtn.onclick = function(){
+  body.classList.toggle('dark');
+}
